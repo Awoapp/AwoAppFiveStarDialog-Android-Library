dbfdf575565f6394d8c418f908194608d3404f20
Awo App <22547869+Awoapp@users.noreply.github.com>
2020-03-28 16:45:37 +0300

    Fix text problem

--- AwoAppFiveStarDialog/src/main/java/com/awoapp/FiveStarDialog/LikedApp.java
+++ AwoAppFiveStarDialog/src/main/java/com/awoapp/FiveStarDialog/LikedApp.java
@@ -15,7 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
